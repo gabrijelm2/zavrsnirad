390a54d5ea88bfe87d9369803409493042b34b1c
gabrijelm2 <gabrijel62f@gmail.com>
2026-05-13 09:19:28 +0200

    Update category.c

--- category.c
+++ category.c
@@ -1,22 +1,28 @@
 /*
-  category.c - Logika za kategorije ribolovne opreme
+  category.c
+  Upravljanje kategorijama proizvoda
 */
 
 #include "header.h"
 
-// Pretvaranje numericke vrijednosti enuma u citljivi tekst
 const char* category_get_name(int categoryId) {
     switch (categoryId) {
-        case CAT_STAPOVI: return "Stapovi";
-        case CAT_ROLE:    return "Role";
-        case CAT_NAJLON:  return "Najlon";
-        case CAT_UDICE:   return "Udice";
-        case CAT_HRANA:   return "Hrana";
-        default:          return "Nepoznata";
+    case CAT_STAPOVI: return "Stapovi";
+    case CAT_ROLE:    return "Role";
+    case CAT_NAJLON:  return "Najlon";
+    case CAT_UDICE:   return "Udice";
+    case CAT_HRANA:   return "Hrana";
+    default:          return "Nepoznata";
     }
 }
 
-// Provjera je li unesena kategorija unutar dozvoljenih granica
 int is_valid_category(int categoryId) {
     return (categoryId >= 0 && categoryId < CAT_COUNT);
+}
+
+void print_all_categories(void) {
+    printf("\nDostupne kategorije:\n");
+    for (int i = 0; i < CAT_COUNT; ++i) {
+        printf("%d. %s\n", i, category_get_name(i));
+    }
 }
